595d53e5c6fd6117b6fb20b93d19e8a2bee49779
eferoghenej <eferoghenej@gmail.com>
2022-11-07 17:19:01 +0100

    client port update commit

--- client/src/config.ts
+++ client/src/config.ts
@@ -7,5 +7,5 @@ export const authConfig = {
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-oc0b7eilgwaowlgo.us.auth0.com',            // Auth0 domain
   clientId: '1ZtVcgxYdx52xXB6bEFzt33YZOC1SK9B',          // Auth0 client id
-  callbackUrl: 'http://localhost:3001/callback'
+  callbackUrl: 'http://localhost:3000/callback'
 }
